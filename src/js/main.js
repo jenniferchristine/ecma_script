198450044ec970bb110931d960503c13a91779fc
jenniferchristine <jeja2306@student.miun.se>
2024-01-27 22:05:52 +0100

    Funktion för att skapa element och skriva ut url-info till webbplats

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,36 @@ window.onload = async () => {
 
     if (response.ok) {
         let data = await response.json();
+
+        printTable(data);
+
     } else {
         console.log("ERROR: " + response.statusText);
     }
+}
+
+function printTable(courses) {
+    const tbodyEl = document.getElementById("table");
+    tbodyEl.innerHTML = "";
+
+    courses.forEach((course) => {
+        const trEl = document.createElement("tr");
+        const codeEl = document.createElement("td");
+        const codeText = document.createTextNode(course.code);
+        codeEl.appendChild(codeText);
+
+        const nameEl = document.createElement("td");
+        const nameText = document.createTextNode(course.coursename);
+        nameEl.appendChild(nameText);
+
+        const progEl = document.createElement("td");
+        const progText = document.createTextNode(course.progression);
+        progEl.appendChild(progText);
+
+        trEl.appendChild(codeEl);
+        trEl.appendChild(nameEl);
+        trEl.appendChild(progEl);
+
+        tbodyEl.appendChild(trEl);
+    });
 }
