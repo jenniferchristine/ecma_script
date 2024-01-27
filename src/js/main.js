14ce56c31eef955eab069943b88621e905dc1bd7
jenniferchristine <jeja2306@student.miun.se>
2024-01-27 22:02:09 +0100

    Onload-funktion för att ladda startsida och hämta info från url

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+window.onload = async () => {
+    const url = "https://dahlgren.miun.se/ramschema_ht23.php";
+    const response = await fetch(url);
+
+    if (response.ok) {
+        let data = await response.json();
+    } else {
+        console.log("ERROR: " + response.statusText);
+    }
+}
